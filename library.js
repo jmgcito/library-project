a51659c8527bf7d340623a440043ba52e5849c5e
Joshua Guillen <jmgmac@Joshuas-MacBook-Pro.local>
2022-07-12 14:30:43 -0700

    Added functions diplayLibrary(), addBooktoDOM() that allow for library to be displayed. Also added addbook button that adds an individual new book to library and dom.

--- library.js
+++ library.js
@@ -20,8 +20,7 @@ function Book(title, author, pages, read) {
   };
 }
 
-theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-
+//prompts user to enter book information
 function addBooktoLibrary() {
   let valid = true;
   let title = prompt("Book Title: ");
@@ -58,7 +57,41 @@ function addBooktoLibrary() {
 
   let newBook = new Book(title, author, pages, read);
   library.push(newBook);
+  return newBook;
 }
 
-addBooktoLibrary();
-console.log(library[0].info());
+//adds individual books to dom
+const libraryContainer = document.querySelector("#library-container");
+function addBooktoDOM(currBook) {
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  p.innerText = currBook.info();
+  div.appendChild(p);
+  //use insert before so newest title appears at top
+  libraryContainer.insertBefore(div, libraryContainer.firstChild);
+}
+
+// loops through array to display books on dom
+//ran only once when page is first loaded
+function displayLibrary() {
+  for (const currBook of library) {
+    addBooktoDOM(currBook);
+  }
+}
+
+const addBook = document.querySelector("#add-book");
+addBook.addEventListener("click", () => {
+  //prompts user to add book, new book is added to library, then added to dom
+  // does not require rerenderering entire library on dom
+  addBooktoDOM(addBooktoLibrary());
+});
+
+theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+ocarina = new Book("Ocarina of Time", "Akira Himekawa", 370, false);
+catcher = new Book("Catcher in the Rye", "J.D. Salinger", 250, true);
+goblet = new Book("The Goblet of Fire", "J.K. Rowling", 634, true);
+library.push(theHobbit);
+library.push(ocarina);
+library.push(catcher);
+library.push(goblet);
+displayLibrary();
